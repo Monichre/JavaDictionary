cbd242e822b2adbdd48eb3709708e1ecc3917a3e
Liam Ellis <liamhellis@gmail.com>
2016-04-29 08:41:39 -0700

    basic layout, two requried classes

---
+++ src/main/java/Definition.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Definition {
+
+	public Definition{
+		
+	}
+	
+}
---
+++ src/main/java/Word.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Word {
+
+	private ArrayList<Definition> definitions = new ArrayList<Definition>();
+	
+	public Word(){
+		
+
+	}
+
+	
+}
