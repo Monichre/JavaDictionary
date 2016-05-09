13ebdee886ce9534dc7fe2c4d0429d2e364fbd3b
Liam Ellis <liamhellis@gmail.com>
2016-05-08 18:50:13 -0700

    indentation tweaks

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,43 +11,42 @@ public class App {
     String layout = "templates/layout.vtl";
 
 	get("/", (request, response) -> {
-	ArrayList<Word> words = Word.getAll();
-	HashMap model = new HashMap();
-	model.put("words", words);
-	model.put("template", "templates/home.vtl");
-	return new ModelAndView(model, layout);
+		ArrayList<Word> words = Word.getAll();
+		HashMap model = new HashMap();
+		model.put("words", words);
+		model.put("template", "templates/home.vtl");
+		return new ModelAndView(model, layout);
 	}, new VelocityTemplateEngine());
 
 	post("/words", (request, response) -> {
-	ArrayList<Word> words = Word.getAll();
-	HashMap model = new HashMap();
-	String inputWord = request.queryParams("word");
-	String inputDef = request.queryParams("definition");
-	Definition definition = new Definition(inputDef);
-	Word word = new Word(inputWord, definition); 
-	model.put("words", words);
-	model.put("template", "templates/home.vtl");
-	return new ModelAndView(model, layout);
+		ArrayList<Word> words = Word.getAll();
+		HashMap model = new HashMap();
+		String inputWord = request.queryParams("word");
+		String inputDef = request.queryParams("definition");
+		Definition definition = new Definition(inputDef);
+		Word word = new Word(inputWord, definition); 
+		model.put("words", words);
+		model.put("template", "templates/home.vtl");
+		return new ModelAndView(model, layout);
 	}, new VelocityTemplateEngine());
 
 	get("/definitions/:id", (request, response) -> {
-	HashMap model = new HashMap();
-	Word word = Word.find(Integer.parseInt(request.params(":id")));
-	model.put("word", word);
-	model.put("template", "templates/definitions.vtl");
-	return new ModelAndView(model, layout);
+		HashMap model = new HashMap();
+		Word word = Word.find(Integer.parseInt(request.params(":id")));
+		model.put("word", word);
+		model.put("template", "templates/definitions.vtl");
+		return new ModelAndView(model, layout);
 	}, new VelocityTemplateEngine());
 
 	post("/anotherDefinition/:id", (request, response) -> {
-	Word word = Word.find(Integer.parseInt(request.params(":id")));
-	HashMap model = new HashMap();
-	String newDefinition = request.queryParams("anotherDefinition");
-	Definition anotherDefinition = new Definition(newDefinition);
-	word.addDefinition(anotherDefinition);
-
-	model.put("word", word);
-	model.put("template", "templates/definitions.vtl");
-	return new ModelAndView(model, layout);
+		Word word = Word.find(Integer.parseInt(request.params(":id")));
+		HashMap model = new HashMap();
+		String newDefinition = request.queryParams("anotherDefinition");
+		Definition anotherDefinition = new Definition(newDefinition);
+		word.addDefinition(anotherDefinition);
+		model.put("word", word);
+		model.put("template", "templates/definitions.vtl");
+		return new ModelAndView(model, layout);
 	}, new VelocityTemplateEngine());
 	
 }
